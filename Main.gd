fc148677566b80862b48daf2906d6f07d5129ed5
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-03-08 17:05:11 +0100

    mleko

--- Main.gd
+++ Main.gd
@@ -1,8 +1,18 @@
 extends Node
 
-#credits http://bevouliin.com/ player sprite
 
 
+# DOCS
+
+# HyperLog https://github.com/GuyUnger/HyperLog
+# Godot Sheets https://github.com/peterhoglund/gd-sheets
+# Godot SFXR https://github.com/tomeyro/godot-sfxr
+
+# Theme: NO COMBAT
+# Plot: Gracz to stary ktory wyszedl po mleko bo brakuje na chacie
+
+# Credits
+# player sprite http://bevouliin.com/ 
 
 
 func _ready():
--- scenes/menu/Menu.gd
+++ scenes/menu/Menu.gd
@@ -1,11 +1,5 @@
 extends Control
 
-# DOCS
-
-# HyperLog https://github.com/GuyUnger/HyperLog
-# Godot Sheets https://github.com/peterhoglund/gd-sheets
-# Godot SFXR https://github.com/tomeyro/godot-sfxr
-
 
 func _ready():
 	var Maps = GDSheets.sheet("Maps")
