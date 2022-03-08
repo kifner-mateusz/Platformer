e9d3ad3236fa8bd8a7022e3957f022ec6e70097c
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-03-08 16:22:24 +0100

    docs & tests

--- scenes/menu/Menu.gd
+++ scenes/menu/Menu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+# DOCS
+
+# HyperLog https://github.com/GuyUnger/HyperLog
+# Godot Sheets https://github.com/peterhoglund/gd-sheets
+# Godot SFXR https://github.com/tomeyro/godot-sfxr
+
+
 func _ready():
 	var Maps = GDSheets.sheet("Maps")
 	print(Maps["1"])
